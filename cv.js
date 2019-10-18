2927a3c139e08c3ab7bb12022a9b1479d970f38a
tx <1031419595@qq.com>
2019-10-18 19:40:04 +0800

    提交代码

---
+++ cv.js
@@ -0,0 +1,111 @@
+let index=0;
+let cv=null;
+
+$(function(){
+   window.onmousewheel=scrollBarMouseWheel();
+
+  windowSize();
+
+  /*监听窗口事件*/
+  window.onresize = function () {
+    windowSize();
+  }
+
+  initEvents();
+})
+
+function initEvents(){
+
+    $(".container-core-nav>li").click(function(){
+      index=$(this).index();
+      containerSlide(-index*$(".section").innerHeight()+"px");
+    })
+
+    let slideIndex=0;
+    let totalSlideIndex=$(".decoration-slide-down>li").length;
+
+    setInterval(function(){
+      let $itemLi= $(".decoration-slide-down>li");
+      $itemLi.eq(slideIndex).removeClass("active");
+      slideIndex++;
+      slideIndex=slideIndex%totalSlideIndex;
+      $itemLi.eq(slideIndex).addClass("active");
+      $itemLi=null;
+    },500);
+
+
+
+    $(".factions-cv-btn").click(function(){
+    let $item=$(this).parent(".factions-cv");
+    cv=new Player($item.children("video"));
+    cv.initCV(function(){
+      initCV($item);
+    })
+    cv.releaseCV(function(){
+      releaseCV($item);
+      $item=null;
+    })
+  })
+
+  function initCV($item){
+    $item.children("img").hide();
+    $item.children(".factions-cv-btn").children("img").hide();
+  }
+
+   function releaseCV($item){
+     $item.children("img").show();
+     $item.children(".factions-cv-btn").children("img").show();
+   }
+
+}
+
+/*窗口滚动*/
+function containerSlide(containerPosition) {
+  $(".container-core").css("top", containerPosition);
+  let $containerCoreNav=$(".container-core-nav");
+  if(index===3){
+    $containerCoreNav.addClass("container-core-nav-reverse");
+  }else{
+    $containerCoreNav.removeClass("container-core-nav-reverse");
+  }
+  $containerCoreNav.children(".active").removeClass("active");
+  $containerCoreNav.children("li").eq(index).addClass("active");
+  if(cv){
+    cv.pauseCV();
+  }
+  $containerCoreNav=null;
+ }
+
+/*窗口变化*/
+function windowSize() {
+  let height = document.documentElement.clientHeight;
+  $(".section").css("height", "" + height + "px");
+}
+
+/*鼠标滑轮滚动*/
+function scrollBarMouseWheel(delay) {
+  let totalIndex = document.querySelectorAll(".section").length;
+  let delta;
+  let timer = null;
+
+
+  return function (e) {
+    clearTimeout(timer);
+    delta=e.wheelDelta?e.wheelDelta:-(e.detail);
+    timer = setTimeout(function () {
+      if (delta < 0) {
+        if (index < totalIndex - 1) {
+           console.log("下滑");
+           index++;
+        }
+      } else {
+          console.log("上滑");
+        if (index != 0) {
+         index--;
+        }
+      }
+
+      containerSlide(-index*$(".section").innerHeight()+"px");
+    }, delay | 300)
+  }
+}
--- player.js
+++ player.js
@@ -12,12 +12,14 @@
     },
     videoCover:function($cover){
       this.player.oncanplaythrough=function () {
+        console.log("视频加载完毕");
         $cover.css("opacity","0");
       }
     },
     selectPV:function(index){
       this.player.src=this.pv[index];
     },
+
     play:function(){
       this.player.play();
     },
@@ -26,10 +28,35 @@
         fn();
       }
     },
+
+
+    initCV:function(fn){
+      fn();
+      if(!this.player.autoplay){
+        this.player.autoplay="autoplay";
+      }
+      this.$player.attr("controls","controls");
+      this.player.muted=false;
+      this.player.play();
+    },
+    releaseCV:function(fn){
+      let it_self=this;
+      this.player.onended=function () {
+        it_self.stop();
+        it_self.$player.removeAttr("controls");
+        fn();
+      }
+    },
+    pauseCV:function(){
+        this.player.pause();
+    },
+
+
     stop:function(){
       this.player.currentTime=0;
       this.player.pause();
     }
+
   }
   Player.prototype.init.prototype=Player.prototype;
   window.Player=Player;
